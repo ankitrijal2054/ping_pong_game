751fdeeb96c678974b2f03ec78e51047f5c7bd1d
ankitrijal2054 <ankitrijal2054@gmail.com>
2021-02-09 12:19:11 -0600

    ball created bounce function created

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ LEFT_COR = (-375, 0)
 
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
@@ -12,13 +14,27 @@ screen.tracer(0)
 
 r_paddle = Paddle(RIGHT_COR)
 l_paddle = Paddle(LEFT_COR)
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 
 while game_is_on:
-    screen.listen()
-    screen.onkey(r_paddle.up, "Up")
-    screen.onkey(r_paddle.down, "Down")
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # detect collision with the wall
+    ball.check_wall_collision()
+
+    # detect collision with paddle
+    if ball.xcor() > 350 and ball.distance(r_paddle) < 50 or ball.xcor() < -350 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
+
 
 screen.exitonclick()
---
+++ venv/ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x = 10
+        self.y = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x
+        new_y = self.ycor() + self.y
+        self.goto(new_x,new_y)
+
+    def bounce_x(self):
+        self.x *= -1
+
+    def bounce_y(self):
+        self.y *= -1
+
+    def check_wall_collision(self):
+        if self.ycor() > 280 or self.ycor() < -280:
+            self.bounce_y()
+
